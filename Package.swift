12e14a5e028c8c2bc7927afa608000c736fe5f76
Linus Långberg <phoqe@phoqe.me>
2023-10-13 10:48:53 +0200

    change reqs

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Henny",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v16),
         .macOS(.v13),
         .tvOS(.v12),
         .driverKit(.v19),
