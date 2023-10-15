c29506695cac98db5d367e8e1dd55c794393fd26
Linus Långberg <phoqe@phoqe.me>
2023-10-15 12:05:04 +0200

    add md support for bio

--- Sources/Henny/API/Models/HNUser.swift
+++ Sources/Henny/API/Models/HNUser.swift
@@ -1,17 +1,18 @@
 import Foundation
+import SwiftUI
 
 public struct HNUser: Codable {
     public let username: String
     public let joined: Date
     public let karma: Int
-    public let bio: String?
+    public let bioHTML: String?
     public let submissions: [Int]?
 
     private enum CodingKeys: String, CodingKey {
         case username = "id"
         case joined = "created"
         case karma
-        case bio = "about"
+        case bioHTML = "about"
         case submissions = "submitted"
     }
 }
@@ -19,5 +20,26 @@ public struct HNUser: Codable {
 public extension HNUser {
     var hnURL: URL {
         return URL(string: "\(HNURL.website)/user?id=\(username)")!
+    }
+    
+    var bioMarkdown: LocalizedStringKey? {
+        guard let bioHTML else {
+            return nil
+        }
+        
+        var bioMarkdown = bioHTML
+        
+        bioMarkdown = bioMarkdown
+            .replacingOccurrences(of: "<i>", with: "*")
+            .replacingOccurrences(of: "</i>", with: "*")
+
+            .replacingOccurrences(of: "<b>", with: "**")
+            .replacingOccurrences(of: "</b>", with: "**")
+
+            .replacingOccurrences(of: "<p>", with: "\n\n")
+        
+        let cleanBioMarkdown = LocalizedStringKey(bioMarkdown)
+        
+        return cleanBioMarkdown
     }
 }
