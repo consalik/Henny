0dce8f54b238ef7372999c5b97b8a8202278418a
Linus Långberg <phoqe@phoqe.me>
2023-10-17 14:24:13 +0200

    add cust coding for user

--- Sources/Henny/API/Models/HNUser.swift
+++ Sources/Henny/API/Models/HNUser.swift
@@ -6,7 +6,7 @@ public struct HNUser: Codable {
     public let joined: Date
     public let karma: Int
     public let bioHTML: String?
-    public let submissionsIds: [Int]?
+    public let submissionsIds: [Int]
 
     private enum CodingKeys: String, CodingKey {
         case username = "id"
@@ -14,6 +14,36 @@ public struct HNUser: Codable {
         case karma
         case bioHTML = "about"
         case submissionsIds = "submitted"
+    }
+    
+    init(username: String, joined: Date, karma: Int, bioHTML: String?, submissionsIds: [Int]) {
+        self.username = username
+        self.joined = joined
+        self.karma = karma
+        self.bioHTML = bioHTML
+        self.submissionsIds = submissionsIds
+    }
+}
+
+public extension HNUser {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        username = try container.decode(String.self, forKey: .username)
+        joined = try container.decode(Date.self, forKey: .joined)
+        karma = try container.decode(Int.self, forKey: .karma)
+        bioHTML = try container.decodeIfPresent(String.self, forKey: .bioHTML)
+        submissionsIds = try container.decodeIfPresent([Int].self, forKey: .submissionsIds) ?? []
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(username, forKey: .username)
+        try container.encode(joined, forKey: .joined)
+        try container.encode(karma, forKey: .karma)
+        try container.encodeIfPresent(bioHTML, forKey: .bioHTML)
+        try container.encodeIfPresent(submissionsIds, forKey: .submissionsIds)
     }
 }
 
