461819c23e36f7ed91cd0b0a01ee77e617be3dc6
Linus Långberg <phoqe@phoqe.me>
2023-10-20 11:43:53 +0200

    add logging

--- Sources/Henny/HNMetadataCache.swift
+++ Sources/Henny/HNMetadataCache.swift
@@ -1,34 +1,91 @@
 import Foundation
 import LinkPresentation
+import OSLog
 
 struct HNMetadataCache {
     
     private let fileManager = FileManager.default
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: String(describing: HNMetadataCache.self))
     
     private func fileURL(for url: URL) throws -> URL {
         let fileName = url.absoluteString.hashValue
         let fileNameString = String(fileName)
         
-        return try fileManager
-            .url(for: .cachesDirectory,
-                 in: .userDomainMask,
-                 appropriateFor: nil,
-                 create: false)
-            .appendingPathComponent("HNMetadataCache")
-            .appendingPathComponent(fileNameString)
+        do {
+            return try fileManager
+                .url(for: .cachesDirectory,
+                    in: .userDomainMask,
+                    appropriateFor: nil,
+                    create: false)
+                .appendingPathComponent("HNMetadataCache")
+                .appendingPathComponent(fileNameString)
+        } catch {
+            logger.error("Failed to get file URL for \(url.absoluteString): \(error.localizedDescription)")
+
+            throw error
+        }
     }
     
     func set(_ metadata: LPLinkMetadata, for url: URL) throws {
         let fileURL = try fileURL(for: url)
-        let data = try NSKeyedArchiver.archivedData(withRootObject: metadata, requiringSecureCoding: true)
+        var data: Data?
+
+        do {
+            data = try NSKeyedArchiver.archivedData(withRootObject: metadata, requiringSecureCoding: true)
+        } catch {
+            logger.error("Failed to archive metadata for \(url.absoluteString): \(error.localizedDescription)")
+
+            throw error
+        }
         
-        try data.write(to: fileURL)
+        guard let data else {
+            logger.error("Failed to archive metadata for \(url.absoluteString)")
+
+            return
+        }
+        
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            logger.error("Failed to write metadata for \(url.absoluteString): \(error.localizedDescription)")
+
+            throw error
+        }
     }
 
     func metadata(for url: URL) throws -> LPLinkMetadata? {
         let fileURL = try fileURL(for: url)
-        let data = try Data(contentsOf: fileURL)
-        let metadata = try NSKeyedUnarchiver.unarchivedObject(ofClass: LPLinkMetadata.self, from: data)
+        var data: Data?
+
+        do {
+            data = try Data(contentsOf: fileURL)
+        } catch {
+            logger.error("Failed to read metadata for \(url.absoluteString): \(error.localizedDescription)")
+
+            throw error
+        }
+
+        guard let data else {
+            logger.error("Failed to read metadata for \(url.absoluteString)")
+
+            return nil
+        }
+
+        var metadata: LPLinkMetadata?
+
+        do {
+            metadata = try NSKeyedUnarchiver.unarchivedObject(ofClass: LPLinkMetadata.self, from: data)
+        } catch {
+            logger.error("Failed to unarchive metadata for \(url.absoluteString): \(error.localizedDescription)")
+
+            throw error
+        }
+
+        guard let metadata else {
+            logger.error("Failed to unarchive metadata for \(url.absoluteString)")
+
+            return nil
+        }
         
         return metadata
     }
